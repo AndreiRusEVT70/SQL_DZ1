b863c84ca0eafa678bf7a8f585c71f369ebe5947
AndreiRusEVT70 <123323175+AndreiRusEVT70@users.noreply.github.com>
2023-04-16 21:27:38 +0300

    Add files via upload

---
+++ запрос1.sql
@@ -0,0 +1,24 @@
+select * from "DZ1"
+order by age asc;
+
+select * from "DZ1"
+where weight > 40 and age > 30
+order by id;
+
+select name, age from "DZ1"
+where age > 35
+order by name;
+
+select * from "DZ1"
+where id > 3 and age > 20
+order by id desc;
+
+select * from "DZ1"
+where age is not null 
+order by name desc 
+
+
+
+
+
+
